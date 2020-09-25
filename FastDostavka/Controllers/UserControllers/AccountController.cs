3ba48668b0652e699e55bf8ba3d9ff2a77f359d3
Daniil <danik22122005@gmail.com>
2020-09-25 17:38:20 +0300

    user profile back

---
+++ FastDostavka/Controllers/UserControllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastDostavka.Data;
+using FastDostavka.Data.Entities.IdentityUser;
+using FastDostavka.Services;
+using FastDostavka.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastDostavka.Controllers.UserControllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<DbUser> _userManager;
+        private readonly SignInManager<DbUser> _signInManager;
+        private readonly DBContext _context;
+        private readonly IJwtTokenService _jwtTokenService;
+        public AccountController(DBContext context, UserManager<DbUser> userManager, SignInManager<DbUser> sigInManager,
+            IJwtTokenService jwtTokenService)
+        {
+            _userManager = userManager;
+            _signInManager = sigInManager;
+            _context = context;
+            _jwtTokenService = jwtTokenService;
+        }
+        [HttpGet("profile")]
+        public  IActionResult Profile()
+        {
+            var userId = User.Claims.ToList()[0].Value;
+            var profile = _context.UserProfiles.FirstOrDefault(x => x.Id == userId);
+            return Ok(new ProfileViewModel()
+            {
+                FirstName = profile.FirstName,
+                LastName = profile.LastName,
+                Age = profile.Age ?? 0 ,
+                Ardess = profile.Address,
+                Image = profile.Image,
+                City = profile.City
+            });
+        }
+    }
+}
---
+++ FastDostavka/ViewModels/ProfileViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastDostavka.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Ardess { get; set; }
+        public string City { get; set; }
+        public string Image { get; set; }
+    }
+}
