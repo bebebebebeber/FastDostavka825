936971fd150ec3d73e027d0ff0013b84cf0015c4
Daniil <danik22122005@gmail.com>
2020-10-15 22:48:11 +0300

    create order

---
+++ FastDostavka/ViewModels/OrderViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastDostavka.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string Adress { get; set; }
+        public int House { get; set; }
+        public int Flat { get; set; }
+        public int GoodsId { get; set; }
+
+    }
+}
