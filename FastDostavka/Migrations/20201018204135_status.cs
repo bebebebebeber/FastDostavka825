8c37a7b0334852b982f2bdf02ff1a42dd9f99466
Daniil <danik22122005@gmail.com>
2020-10-19 15:41:39 +0300

    fix

---
+++ FastDostavka/Migrations/20201018204135_status.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FastDostavka.Migrations
+{
+    public partial class status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderStatusId",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "OrderStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderStatusId",
+                table: "Orders",
+                column: "OrderStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_OrderStatuses_OrderStatusId",
+                table: "Orders",
+                column: "OrderStatusId",
+                principalTable: "OrderStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_OrderStatuses_OrderStatusId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "OrderStatuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderStatusId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderStatusId",
+                table: "Orders");
+        }
+    }
+}
