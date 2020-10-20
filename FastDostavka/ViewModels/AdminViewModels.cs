0220ec77638d9f19c730261c7eb5a2c5ba8b226c
Maxym Martynenkow <martymaxon@gmail.com>
2020-10-20 16:28:26 +0300

    add product service

--- FastDostavka/ViewModels/AdminViewModels.cs
+++ FastDostavka/ViewModels/AdminViewModels.cs
@@ -9,7 +9,15 @@ namespace FastDostavka.ViewModels
     public class AdminOrdersViewModel
     {
         public List<OrderModel> Orders { get; set; }
-        public List<OrderStatus> Statuses{ get; set; }
+        public List<OrderStatus> Statuses { get; set; }
         public int Pages { get; set; }
+    }
+    public class AdminAddProductViewModel
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Decription { get; set; }
+        public double Price { get; set; }
+        public int StoreId { get; set; }
     }
 }
